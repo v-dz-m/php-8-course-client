f1c6e96a5909ed34360a0b2132070b87794e2c28
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-11-27 23:48:21 +0300

    Init commit

---
+++ app/controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Main;
+use RedBeanPHP\R;
+use wfm\Controller;
+
+/** @property Main $model */
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+        $names = $this->model->get_names();
+        $spec_name = R::getRow('SELECT * FROM name WHERE id = 2');
+        $this->setMeta('Главная страница', 'Description...', 'keywords...');
+        $this->set(compact('names'));
+    }
+}
