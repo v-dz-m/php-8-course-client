e80a4d9b3802e1886bb36de2af424a566cc9fda0
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-12-05 15:08:45 +0300

    Lesson No. 14
    Add appending items into session (with prototype of cart)

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -8,7 +8,7 @@ use wfm\App;
 /** @property Cart $model */
 class CartController extends AppController
 {
-    public function addAction()
+    public function addAction(): bool
     {
         $lang = App::$app->getProperty('language');
         $id = get('id');
@@ -19,6 +19,16 @@ class CartController extends AppController
         }
 
         $product = $this->model->get_product($id, $lang);
-        debug($product, 1);
+        if (!$product) {
+            return false;
+        }
+
+        $this->model->add_to_cart($product, $qty);
+        if ($this->isAjax()) {
+            debug($_SESSION['cart'], 1);
+        }
+
+        redirect();
+        return true;
     }
 }
--- vendor/wfm/Controller.php
+++ vendor/wfm/Controller.php
@@ -42,4 +42,9 @@ abstract class Controller
             'keywords' => $keywords
         ];
     }
+
+    public function isAjax(): bool
+    {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
+    }
 }
