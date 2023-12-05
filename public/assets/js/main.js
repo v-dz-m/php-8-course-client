90b5a1bf5787a0680de6525583db52029e42274c
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-12-05 15:40:38 +0300

    Lesson No. 15
    Add cart displaying after click on add item button

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,70 +1,75 @@
-$(function() {
+$(function () {
 
-	$('.add-to-cart').on('click', function (e) {
-		e.preventDefault();
-		const id = $(this).data('id');
-		const qty = $('#input-quantity').val() ?? 1;
-		const $this = $(this);
-		console.log(id, qty);
+    function showCart(cart) {
+        $('#cart-modal .modal-cart-content').html(cart);
+        const myModalEl = document.querySelector('#cart-modal');
+        const modal = bootstrap.Modal.getOrCreateInstance(myModalEl);
+        modal.show();
+    }
 
-		$.ajax({
-			url: 'cart/add',
-			type: 'GET',
-			data: {
-				id: id,
-				qty: qty
-			},
-			success: function (res) {
-				console.log(res);
-			},
-			error: function () {
-				alert('Error');
-			}
-		});
-	});
+    $('.add-to-cart').on('click', function (e) {
+        e.preventDefault();
+        const id = $(this).data('id');
+        const qty = $('#input-quantity').val() ? $('#input-quantity').val() : 1;
+        const $this = $(this);
 
-	$('.open-search').click(function(e) {
-		e.preventDefault();
-		$('#search').addClass('active');
-	});
-	$('.close-search').click(function() {
-		$('#search').removeClass('active');
-	});
+        $.ajax({
+            url: 'cart/add',
+            type: 'GET',
+            data: {id: id, qty: qty},
+            success: function (res) {
+                showCart(res)
+            },
+            error: function () {
+                alert('Error!');
+            }
+        });
+    });
 
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 200) {
-			$('#top').fadeIn();
-		} else {
-			$('#top').fadeOut();
-		}
-	});
+    // CART
 
-	$('#top').click(function() {
-		$('body, html').animate({scrollTop:0}, 700);
-	});
+    $('.open-search').click(function (e) {
+        e.preventDefault();
+        $('#search').addClass('active');
+    });
+    $('.close-search').click(function () {
+        $('#search').removeClass('active');
+    });
 
-	$('.sidebar-toggler .btn').click(function() {
-		$('.sidebar-toggle').slideToggle();
-	});
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+    });
 
-	$('.thumbnails').magnificPopup({
-		type:'image',
-		delegate: 'a',
-		gallery: {
-			enabled: true
-		},
-		removalDelay: 500,
-		callbacks: {
-			beforeOpen: function() {
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		}
-	});
+    $('#top').click(function () {
+        $('body, html').animate({scrollTop: 0}, 700);
+    });
 
-	$('#languages button').on('click', function () {
-		const lang_code = $(this).data('langcode');
-		window.location = PATH + '/language/change?lang=' + lang_code;
-	});
+    $('.sidebar-toggler .btn').click(function () {
+        $('.sidebar-toggle').slideToggle();
+    });
+
+    $('.thumbnails').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        },
+        removalDelay: 500,
+        callbacks: {
+            beforeOpen: function () {
+                this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        }
+    });
+
+    $('#languages button').on('click', function () {
+        const lang_code = $(this).data('langcode');
+        window.location = PATH + '/language/change?lang=' + lang_code;
+    });
 
 });
