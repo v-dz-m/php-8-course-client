bbb8900fa933f43af5a50851a91bb3fbc950e0d9
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-12-06 15:56:21 +0300

    Lesson No. 19
    Show product page with actual information about item

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,10 +2,23 @@
 
 namespace app\controllers;
 
+use app\models\Product;
+use wfm\App;
+
+/** @property Product $model */
 class ProductController extends AppController
 {
     public function viewAction()
     {
+        $lang = App::$app->getProperty('language');
+        $product = $this->model->get_product($this->route['slug'], $lang);
 
+        if (!$product) {
+            throw new \Exception("Товар по запросу {$this->route['slug']} не найден", 404);
+        }
+
+        $gallery = $this->model->get_gallery($product['id']);
+        $this->setMeta($product['title'], $product['description'], $product['keywords']);
+        $this->set(compact('product', 'gallery'));
     }
 }
