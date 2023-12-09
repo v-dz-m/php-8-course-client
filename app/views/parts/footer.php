c32d30b2d7310c5dc80ec3dc14213bacdbed30fb
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-12-09 08:29:22 +0300

    Lesson No. 30
    Add widget menu (for pages / information column)

--- app/views/parts/footer.php
+++ app/views/parts/footer.php
@@ -11,12 +11,17 @@ use wfm\View;
             <div class="row">
                 <div class="col-md-3 col-6">
                     <h4><?php __('tpl_information'); ?></h4>
-                    <ul class="list-unstyled">
+                    <?php new \app\widgets\page\Page([
+//                        'cache' => 0,
+                        'class' => 'list-unstyled',
+                        'prepend' => '<li><a href="' . base_url() .'">' . ___('tpl_home_link') .'</a></li>',
+                    ]) ?>
+                    <!--<ul class="list-unstyled">
                         <li><a href="#">Главная</a></li>
                         <li><a href="#">О магазине</a></li>
                         <li><a href="#">Оплата и доставка</a></li>
                         <li><a href="#">Контакты</a></li>
-                    </ul>
+                    </ul>-->
                 </div>
 
                 <div class="col-md-3 col-6">
