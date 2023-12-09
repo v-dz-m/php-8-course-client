0a5071db65805c7689361ae7e8bc7febc888f4c4
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-12-09 10:14:17 +0300

    Lesson No. 32
    Create user model and controller, add data loading for default model

--- vendor/wfm/Model.php
+++ vendor/wfm/Model.php
@@ -13,4 +13,13 @@ abstract class Model
     {
         Db::getInstance();
     }
+
+    public function load($data)
+    {
+        foreach ($this->attributes as $name => $value) {
+            if (isset($data[$name])) {
+                $this->attributes[$name] = $data[$name];
+            }
+        }
+    }
 }
