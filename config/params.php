47d1a04ba5b9b758af9dfd4171e9d1a7a70dd128
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-12-10 22:23:26 +0300

    Lesson No. 39
    Add mail sending after ordering
    Currently not working, need to find free SMTP sender, because former mail server can't send (probably), credentials in params are fake

--- config/params.php
+++ config/params.php
@@ -1,7 +1,15 @@
 <?php
 
 return [
-    'admin_email' => 'admin@new-ishop.loc',
+    'admin_email' => 'admin@client.test',
     'site_name' => 'E-Shop',
     'pagination' => 3,
+
+    'smtp_host' => 'smtp.yandex.ru',
+    'smtp_auth' => true,
+    'smtp_port' => 465,
+    'smtp_username' => 'test@yandex.by',
+    'smtp_password' => 'test1test2test3',
+    'smtp_secure' => 'ssl',
+    'smtp_from_email' => 'test@yandex.by',
 ];
