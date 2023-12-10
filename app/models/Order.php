24ea3a634aca2823c09e7f96fc850a3c17cc52d1
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2023-12-10 21:27:13 +0300

    Lessson No. 38
    Insert info into tables with physical and virtual products after making an order

--- app/models/Order.php
+++ app/models/Order.php
@@ -17,6 +17,7 @@ class Order extends AppModel
             $order->qty = $_SESSION['cart.qty'];
 
             $order_id = R::store($order);
+            self::saveOrderProduct($order_id, $data['user_id']);
 
             R::commit();
             return $order_id;
@@ -24,5 +25,28 @@ class Order extends AppModel
             R::rollback();
             return false;
         }
+    }
+
+    public static function saveOrderProduct($order_id, $user_id): void
+    {
+        $sql_part = '';
+        $binds = [];
+        foreach ($_SESSION['cart'] as $product_id => $product) {
+            if ($product['is_download']) {
+                $download_id = R::getCell("SELECT download_id FROM product_download WHERE product_id = ?", [$product_id]);
+                $order_download = R::xdispense('order_download');
+                $order_download->order_id = $order_id;
+                $order_download->user_id = $user_id;
+                $order_download->product_id = $product_id;
+                $order_download->download_id = $download_id;
+                R::store($order_download);
+            }
+
+            $sum = $product['qty'] * $product['price'];
+            $sql_part .= "(?,?,?,?,?,?,?),";
+            $binds = array_merge($binds, [$order_id, $product_id, $product['title'], $product['slug'], $product['qty'], $product['price'], $sum]);
+        }
+        $sql_part = rtrim($sql_part, ',');
+        R::exec("INSERT INTO order_product (order_id, product_id, title, slug, qty, price, sum) VALUES $sql_part", $binds);
     }
 }
